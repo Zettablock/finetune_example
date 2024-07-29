5ea43163f486e189a94920d332a2185e60eaabeb
aidanwiteck <59320558+aidanwiteck@users.noreply.github.com>
2024-07-29 14:09:12 -0700

    first commit

---
+++ dataset.py
@@ -0,0 +1,5 @@
+from torch.utils.data import Dataset
+
+class SpamDataset(Dataset):
+    def __init__():
+        return
---
+++ prompts.py
@@ -0,0 +1,42 @@
+TEMPLATE_PROMPT = """Below is an email. You must classify whether or not this email is spam. If the email is indeed spam, print "spam". If the email is not spam, print "ham". 
+
+### Subject:
+{}
+
+### Message:
+{}
+
+### Label:
+{}"""
+
+FEWSHOT_PROMPT = """Here are a few examples. 
+
+### Subject:
+Unbelievable Discount on Designer Watches! Just $19.99!
+
+### Message:
+Unlock the secret to luxury at a fraction of the price! Our exclusive collection of designer watches is now available for only $19.99! 
+Don’t miss out on this once-in-a-lifetime offer. Click now to secure your stylish timepiece! #BestDeals #LimitedOffer #DesignerWatches. 
+Quality that exceeds your expectations. Hurry, this offer won’t last long!
+
+### Label:
+spam
+
+
+### Subject:
+Team Meeting Reminder - August 3rd at 10:00 AM
+
+### Message:
+Hi Team,
+
+Just a reminder that we have a project update meeting tomorrow at 10 AM in the conference room.
+
+See you there!
+
+Best,
+Aidan
+
+### Label:
+ham
+
+"""
